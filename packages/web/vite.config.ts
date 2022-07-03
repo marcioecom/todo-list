ce8638c007507b1d5153b10cb56b0c39aadba2cf
Marcio Junior <marcio.mj70@gmail.com>
2022-07-03 02:58:35 -0300

    build: add docker to application

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -4,5 +4,9 @@ import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()]
+  plugins: [react(), eslint()],
+  server: {
+    host: true,
+    port: 3000
+  }
 })
