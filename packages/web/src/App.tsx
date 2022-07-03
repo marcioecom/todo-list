ef9b30c82f4ba6f9665571073c253409f5a14a38
Marcio Junior <marcio.mj70@gmail.com>
2022-07-03 01:31:14 -0300

    feat(web): add header

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -10,30 +10,35 @@ function App() {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
-    <Container maxW="container.lg" mt={8}>
-      <Flex justify="space-between" align="center">
-        <VStack align="flex-start">
-          <Heading>Olá Márcio</Heading>
-          <Text>Você tem {tasks.length} tarefas</Text>
-        </VStack>
+    <Box>
+      <Heading bg="#2FC18C" color="white" textAlign="center" py={2}>
+        Ebytr
+      </Heading>
+      <Container maxW="container.lg" mt={8}>
+        <Flex justify="space-between" align="center">
+          <VStack align="flex-start">
+            <Heading size="lg">Olá Márcio</Heading>
+            <Text>Você tem {tasks.length} tarefas</Text>
+          </VStack>
 
-        <CreateTaskModal isOpen={isOpen} onClose={onClose} />
-        <Button colorScheme="teal" onClick={onOpen}>
-          Criar tarefa
-        </Button>
-      </Flex>
+          <CreateTaskModal isOpen={isOpen} onClose={onClose} />
+          <Button colorScheme="teal" onClick={onOpen}>
+            Criar tarefa
+          </Button>
+        </Flex>
 
-      <Box mt={8}>
-        { tasks.length >= 1
-          ? tasks.map((task) => (
-          <Task
-            key={task.id}
-            task={task}
-          />
-          ))
-          : <Text textAlign="center">Nenhuma tarefa encontrada</Text>}
-      </Box>
-    </Container>
+        <Box mt={8}>
+          { tasks.length >= 1
+            ? tasks.map((task) => (
+            <Task
+              key={task.id}
+              task={task}
+            />
+            ))
+            : <Text textAlign="center">Nenhuma tarefa encontrada</Text>}
+        </Box>
+      </Container>
+    </Box>
   )
 }
 
